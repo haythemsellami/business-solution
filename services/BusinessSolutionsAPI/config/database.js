50c3e52d02afdcd3542a37e1741a46be7d6bb0a3
Haythem Sellami <sellamihaythem96@gmail.com>
2018-06-16 07:49:35 +0100

    database config

---
+++ services/BusinessSolutionsAPI/config/database.js
@@ -0,0 +1,17 @@
+module.exports = (mongoose, config) => {
+    const database = mongoose.connection;
+    mongoose.Promise = Promise;
+    mongoose.connect(config.database, {
+      useMongoClient: true,
+      promiseLibrary: global.Promise
+    });
+    database.on('error', error => console.log(`Connection to business solution database failed: ${error}`));
+    database.on('connected', () => console.log('Connected to business solution database'));
+    database.on('disconnected', () => console.log('Disconnected from business solution database'));
+    process.on('SIGINT', () => {
+      database.close(() => {
+        console.log('business solution terminated, connection closed');
+        process.exit(0);
+      })
+    });
+  };
