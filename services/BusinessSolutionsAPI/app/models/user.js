bc5dc3300eb7e5b913d554295e96ee88074687f4
Haythem Sellami <sellamihaythem96@gmail.com>
2018-06-16 02:10:59 +0100

    encrypt and compare password

--- services/BusinessSolutionsAPI/app/models/user.js
+++ services/BusinessSolutionsAPI/app/models/user.js
@@ -20,3 +20,27 @@ const Schema = mongoose.Schema({
         required: true
     }
 });
+
+Schema.pre('save', function (next) {
+  const user = this;
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(10, (error, salt) => {
+    if (error) return next(error);
+    bcrypt.hash(user.password, salt, (error, hash) => {
+      if (error) return next(error);
+      user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+Schema.methods.comparePassword = function (password, callback) {
+    bcrypt.compare(password, this.password, (error, matches) => {
+      if (error) return callback(error);
+      callback(null, matches);
+    });
+};
+  
