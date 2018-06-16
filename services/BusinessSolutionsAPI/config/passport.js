522470379b1e9526b3135aeed6a6a0f3e57eb3a5
Haythem Sellami <sellamihaythem96@gmail.com>
2018-06-16 07:13:53 +0100

    package.json

---
+++ services/BusinessSolutionsAPI/config/passport.js
@@ -0,0 +1,5 @@
+const PassportJWT = require('passport-jwt'),
+      ExtractJWT = PassportJWT.ExtractJwt,
+      Strategy = PassportJWT.Strategy,
+      config = require('./index.js'),
+      models = require('@BudgetManager/app/setup');
