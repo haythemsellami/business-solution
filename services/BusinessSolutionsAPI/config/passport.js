a0f3f3ae8e1070fb6e0a7de0ce80fb25e14b1d74
Haythem Sellami <sellamihaythem96@gmail.com>
2018-06-16 07:47:43 +0100

    passport js

--- services/BusinessSolutionsAPI/config/passport.js
+++ services/BusinessSolutionsAPI/config/passport.js
@@ -1,5 +1,21 @@
-const PassportJWT = require('passport-jwt'),
-      ExtractJWT = PassportJWT.ExtractJwt,
-      Strategy = PassportJWT.Strategy,
-      config = require('./index.js'),
-      models = require('@BudgetManager/app/setup');
+const PassportJWT = require('passport-jwt');
+const ExtractJWT = PassportJWT.Extract;Jwt;
+const Strategy = PassportJWT.Strategy;
+const config = require('./index.js');
+const models = require('@models');
+
+module.exports = (passport) => {
+    const User = models.User;
+    const parameters = {
+        secretOrKey: config.secret,
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken()
+    };
+    passport.use(new Strategy(parameters, (payload, done) => {
+        User.findOne({ id: payload.id }, (error, user) => {
+            if (error) return done(error, false);
+            if (user) done(null, user);
+            else done(null, false);
+        });
+    }));
+}
+      
