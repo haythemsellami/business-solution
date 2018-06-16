010d67281fab938fc83804849d1baf2d4fd10c1d
Haythem Sellami <sellamihaythem96@gmail.com>
2018-06-16 01:45:38 +0100

    JWT token secret

--- services/BusinessSolutionsAPI/config/index.js
+++ services/BusinessSolutionsAPI/config/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    secret: 'budgetsecret',
+    secret: process.env.JWTSecret,
     session: { session: false },
     database: 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+process.env.DB_HOST
   }
