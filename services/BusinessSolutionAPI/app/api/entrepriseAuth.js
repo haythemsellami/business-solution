4c13605ce1a279e851dbcbcab624a70dc4b8dac2
Haythem Sellami <sellamihaythem96@gmail.com>
2018-06-17 15:44:37 +0100

    fix Entreprise.findOne

--- services/BusinessSolutionAPI/app/api/entrepriseAuth.js
+++ services/BusinessSolutionAPI/app/api/entrepriseAuth.js
@@ -6,7 +6,7 @@ const api = {};
 
 //Login method
 api.login = (Entreprise) => (req, res) => {
-    Entreprise.findOne({ entrepriseName: req.body.entrepriseName }, (error, entreprise) => {
+    mongoose.model('Entreprise').findOne({ entrepriseName: req.body.entrepriseName }, (error, entreprise) => {
         if (error) throw error;
         if (!entreprise) { 
             res.status(401).send({ success: false, message: 'Authentication failed. Entreprise not found.' });
